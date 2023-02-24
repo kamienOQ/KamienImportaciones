11f020873c3f2044c55a37a5d80af37f13e8e02a
Steven <alvaradosteven6@gmail.com>
2023-02-24 17:09:29 -0600

    Create new branch to unpload my task

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { HelloWorldApp } from './HelloWorldApp'
 import './index.css'
+
+// <App />
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HelloWorldApp></HelloWorldApp>
   </React.StrictMode>,
 )
