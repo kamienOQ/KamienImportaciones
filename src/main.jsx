5b8f2a8511e0a5949c588c4b9427a387b089f187
Steven <alvaradosteven6@gmail.com>
2023-03-08 20:46:38 -0600

    Reset the advances because is work in other repository

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
-
-import { App } from './App';
-//import Navbar from './Components/Navbar';
-import './Firebase/Firebase';
-import 'bootswatch/dist/superhero/bootstrap.min.css';
-import './styles.css';
+import App from './App'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 )
