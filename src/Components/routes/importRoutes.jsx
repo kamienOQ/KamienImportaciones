92a74450126a31c39340b879207b2f0d7a4f5134
Steven <alvaradosteven6@gmail.com>
2023-05-15 23:01:39 -0600

    Changes in filter to integrate to ProductPage

--- src/Components/routes/importRoutes.jsx
+++ src/Components/routes/importRoutes.jsx
@@ -1,9 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { MasterPage } from "../MasterPage";
 import { CategoriesPage } from "../../Pages/CategoriesPage";
-import { AboutPage } from "../../Pages/AboutPage";
 import { ProductsPage } from "../../Pages/ProductsPage";
-import { ProductsModalDetail } from "../products/ProductsModalDetail";
 
 export const ImportRoutes = () => {
   return (
@@ -12,10 +10,10 @@ export const ImportRoutes = () => {
      <Routes>
        {/* <Route path="/" element={<MasterPage />} /> */}
        <Route path="/" element={<CategoriesPage />} />
-       <Route path="/ProductoDetalle" element={<ProductsModalDetail />} />
-       <Route path= "/Producto" element = {<ProductsPage />}/>
-       <Route path= "/Categorias" element = {<CategoriesPage/>}/>
-       <Route path= "/Nosotros" element = {<AboutPage/>}/>
+       <Route path="/Categorias" element={<CategoriesPage />} />
+       <Route path= "/Productos" element = {<ProductsPage />}/>
+       {/*<Route path= "/Categorias" element = {<CategoriesCrud/>}/>
+       <Route path= "/Atributos" element = {<AttibutesCrud/>}/> */}
        <Route path="/*" element={<Navigate to="/" />} />
      </Routes>
     </MasterPage>
