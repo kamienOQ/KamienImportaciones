ba18303235edf86f82ba3009e7ba0dd15a4a0cc4
Steven Alvarado Aguilar <alvaradosteven6@gmail.com>
2024-04-09 04:37:24 -0600

    Update change importants changes in svg with errors to update

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -7,24 +7,24 @@ import { getStorage } from "firebase/storage";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAJu-3VPPHsgx8pMJTffmwYb9RXoXkh_jE",
-  authDomain: "project-kamien.firebaseapp.com",
-  projectId: "project-kamien",
-  storageBucket: "project-kamien.appspot.com",
-  messagingSenderId: "237921208573",
-  appId: "1:237921208573:web:b18d13984139377091d694"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAJu-3VPPHsgx8pMJTffmwYb9RXoXkh_jE",
+//   authDomain: "project-kamien.firebaseapp.com",
+//   projectId: "project-kamien",
+//   storageBucket: "project-kamien.appspot.com",
+//   messagingSenderId: "237921208573",
+//   appId: "1:237921208573:web:b18d13984139377091d694"
+// };
 
 // Produccion Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDQmUwAf5t9_lYYJZD9IEPuRYzkhFzlbB8",
-//   authDomain: "project-kamien-main.firebaseapp.com",
-//   projectId: "project-kamien-main",
-//   storageBucket: "project-kamien-main.appspot.com",
-//   messagingSenderId: "89353296900",
-//   appId: "1:89353296900:web:f9e82eb4c59d960961e951"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyDQmUwAf5t9_lYYJZD9IEPuRYzkhFzlbB8",
+  authDomain: "project-kamien-main.firebaseapp.com",
+  projectId: "project-kamien-main",
+  storageBucket: "project-kamien-main.appspot.com",
+  messagingSenderId: "89353296900",
+  appId: "1:89353296900:web:f9e82eb4c59d960961e951"
+};
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig);
