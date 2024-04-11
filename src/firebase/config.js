c8229c641fe36a035e4b58ff5dbdb33b3541f0d5
Steven Alvarado Aguilar <alvaradosteven6@gmail.com>
2024-04-11 12:20:55 -0600

    Encapsulation of the segurity of the project create a .env.local to differents importants data and modificate the name

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,6 +3,21 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+
+const apiKeyMain =import.meta.env.VITE_APP_API_KEY_MAIN;
+const authDomainMain =import.meta.env.VITE_APP_API_AUTHDOMAIN_MAIN;
+const projectIdMain =import.meta.env.VITE_APP_API_PROJECTID_MAIN;
+const storageBucketMain =import.meta.env.VITE_APP_API_STORAGEBUCKET_MAIN;
+const messagingSenderIdMain =import.meta.env.VITE_APP_API_MESSAGINGSENDERID_MAIN;
+const appIdMain =import.meta.env.VITE_APP_API_APPID_MAIN; 
+
+const apiKey =import.meta.env.VITE_APP_API_KEY;
+const authDomain =import.meta.env.VITE_APP_API_AUTHDOMAIN;
+const projectId =import.meta.env.VITE_APP_API_PROJECTID;
+const storageBucket =import.meta.env.VITE_APP_API_STORAGEBUCKET;
+const messagingSenderId =import.meta.env.VITE_APP_API_MESSAGINGSENDERID;
+const appId =import.meta.env.VITE_APP_API_APPID; 
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,12 +33,12 @@ import { getStorage } from "firebase/storage";
 
 // Produccion Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDQmUwAf5t9_lYYJZD9IEPuRYzkhFzlbB8",
-  authDomain: "project-kamien-main.firebaseapp.com",
-  projectId: "project-kamien-main",
-  storageBucket: "project-kamien-main.appspot.com",
-  messagingSenderId: "89353296900",
-  appId: "1:89353296900:web:f9e82eb4c59d960961e951"
+  apiKey: apiKeyMain,
+  authDomain: authDomainMain,
+  projectId: projectIdMain,
+  storageBucket: storageBucketMain,
+  messagingSenderId: messagingSenderIdMain,
+  appId: appIdMain
 };
 
 // Initialize Firebase
