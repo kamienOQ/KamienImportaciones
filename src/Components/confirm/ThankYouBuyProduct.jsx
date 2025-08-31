ce99b6b1ada5235d455de4af10dfe5fb62ee524c
StevenAlvaradoAguilar <alvaradosteven6@gmail.com>
2025-08-31 15:10:43 -0600

    New changes at the icon logo and page name

--- src/Components/confirm/ThankYouBuyProduct.jsx
+++ src/Components/confirm/ThankYouBuyProduct.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import KamienGif from '../../assets/KámienStore.gif';
+import ChayGif from '../../assets/ChayThanks.gif';
 import { Typography } from '@mui/material';
 import { onChangeSuccess } from '../../store/buying/buyingSlice';
 import { onCleanProducts } from '../../store/cart/cartSlice';
@@ -17,7 +17,7 @@ const ThankYouBuyProduct = () => {
                 <div className='thanks-title-container'>
                     <Typography variant="h3">Muchas gracias por la compra de los productos de ChayCR</Typography>
                 </div>
-                <img src={KamienGif} alt="Gif de agradecimiento" />
+                <img src={ChayGif} alt="Gif de agradecimiento" />
             </div>
         </div>
     )
