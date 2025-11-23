6d4dc94ae0fb0b4a5ae931d77a88fdb74bcabf1e
StevenAlvaradoAguilar <alvaradosteven6@gmail.com>
2025-11-23 10:37:31 -0600

    New changes

--- src/store/products/productsSlice.js
+++ src/store/products/productsSlice.js
@@ -1,67 +1,65 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const productsSlice = createSlice({
-    name: 'products',
-    initialState: {
-        products: [],
-        message: {      
-            error: '',
-            success: ''
-        },
-        activeProduct: null,
+  name: "products",
+  initialState: {
+    products: [],
+    message: {
+      error: "",
+      success: "",
     },
-    reducers: {
-        onSetActiveProduct: ( state, { payload } ) => {
-            state.activeProduct = payload;
-        },
-        onSetProducts: ( state, { payload } ) => {
-            state.products = payload;
-        },
-        onChargeProduct: ( state, { payload } ) => {
-            let duplicate = false
-            if(state.products){
-                state.products.forEach(product => {
-                    if(product.productName === payload.productName)
-                        duplicate = true
-                });
-                if(!duplicate){
-                    state.products.push( payload );
-                }
-            }else{
-                state.products.push( payload );
-            }
-        },
-        onCleanProducts: ( state ) => {
-            state.products = [];
-        },
-        onSetProductSelected: ( state, { payload } ) => {
-            state.productsSelected = payload;
-        },
-        onAddErrorMessage: ( state, { payload } ) => {
-            state.message.error = payload;
-        },
-        onAddSuccessMessage: ( state, { payload } ) => {
-            state.message.success = payload;
-        },
-        onSetProductsByCategorySelect: ( state, { payload } ) => {
-            state.productsSelected = payload;
-        },
-        onCleanActiveProduct: ( state ) => {
-            state.activeProduct = null;
-        },
-    }
+    activeProduct: null,
+  },
+  reducers: {
+    onSetActiveProduct: (state, { payload }) => {
+      state.activeProduct = payload;
+    },
+    onSetProducts: (state, { payload }) => {
+      state.products = payload;
+    },
+    onChargeProduct: (state, { payload }) => {
+      let duplicate = false;
+      if (state.products) {
+        state.products.forEach((product) => {
+          if (product.productName === payload.productName) duplicate = true;
+        });
+        if (!duplicate) {
+          state.products.push(payload);
+        }
+      } else {
+        state.products.push(payload);
+      }
+    },
+    onCleanProducts: (state) => {
+      state.products = [];
+    },
+    onSetProductSelected: (state, { payload }) => {
+      state.productsSelected = payload;
+    },
+    onAddErrorMessage: (state, { payload }) => {
+      state.message.error = payload;
+    },
+    onAddSuccessMessage: (state, { payload }) => {
+      state.message.success = payload;
+    },
+    onSetProductsByCategorySelect: (state, { payload }) => {
+      state.productsSelected = payload;
+    },
+    onCleanActiveProduct: (state) => {
+      state.activeProduct = null;
+    },
+  },
 });
 
-
 // Action creators are generated for each case reducer function
-export const { 
-    onSetActiveProduct, 
-    onSetProducts, 
-    onSetProductSelected, 
-    onAddErrorMessage,
-    onAddSuccessMessage,
-    onSetProductsByCategorySelect,
-    onCleanActiveProduct,
-    onChargeProduct,
-    onCleanProducts
+export const {
+  onSetActiveProduct,
+  onSetProducts,
+  onSetProductSelected,
+  onAddErrorMessage,
+  onAddSuccessMessage,
+  onSetProductsByCategorySelect,
+  onCleanActiveProduct,
+  onChargeProduct,
+  onCleanProducts,
 } = productsSlice.actions;
